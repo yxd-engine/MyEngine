5f74fa12f80e2876ffafa2cd4dcb9f6490c7b653
yxd <yxdaysthj@outlook.com>
2025-07-04 17:33:44 +0800

    first commit

---
+++ MyEngine/MyEngine.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <glad/gl.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
